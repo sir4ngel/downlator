5b36dc3b4f073a3df10d099e850a95d571c10db6
Angel Garcia <angel_dogui@hotmail.com>
2020-12-05 14:42:58 -0600

    Se definio el lugar de cada componente y se creo la estructura basica de la logica

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,101 +14,64 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  Button,
+  Alert,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      mbits: 0,
+      fileSize: 0,
+      r: 0
+    };
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+  handleText(n, inputN) {
+    if(Number.isInteger(+n)){
+      inputN == 1
+      ? this.setState({mbits: +n})
+      : this.setState({fileSize: +n});
+    }
+    
+}
+
+  handleOperation() {
+    this.setState({r: this.state.mbits + this.state.fileSize});
+  }
+  render()
+  {
+    const {r, mbits} = this.state;
+    return(
+      <View style={{flex: 1, backgroundColor: 'white', flexDirection: 'column'}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+          <Text>DOWNLATOR - Logo</Text>
+        </View>
+        <View style={{flex: 5, backgroundColor: 'skyblue'}}>
+          <View>
+            <TextInput placeholder="Megabits" onChangeText={(a) => this.handleText(a, 1)} style={{borderColor: 'black', borderWidth: 1}}></TextInput>
+            <TextInput placeholder="File Size" onChangeText={(a) => this.handleText(a, 2)} style={{borderColor: 'black', borderWidth: 1}}></TextInput>
+            <Button title="Calculate" onPress={() => this.handleOperation()}></Button>
+            <Text>El tiempo de descarga es: {this.state.r}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+        </View>
+        <View style={{flex: 1, backgroundColor: 'steelblue'}}>
+          <Text>Anuncios</Text>
+        </View>
+      </View>
+      );
+    }
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  titles: {
+    flex: 1,
+    justifyContent: "center"
+  }
 });
+
 
 export default App;
